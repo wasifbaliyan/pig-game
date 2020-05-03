78a466f9954c77dcca4dfbf500d432d4c5b0d2d7
Wasif Baliyan <w.baliyan@gmail.com>
2020-05-03 13:53:54 +0530

    add game logic and css styles

--- js/app.js
+++ js/app.js
@@ -14,21 +14,74 @@ GAME RULES:
 
 let activePlayer = 0;
 const scores = [0, 0];
-let playerOneScore = 0;
-let playerTwoScore = 0;
+const globalScores = [0, 0];
+const winningScore = 20;
+let isPlaying = true;
+
 document.querySelector(".dice-pic").style.display = "none";
 
 document.querySelector(".roll-dice").addEventListener("click", () => {
-  let dice = Math.floor(Math.random() * 6) + 1;
-  if (dice !== 1) {
-    document.querySelector(".dice-pic").style.display = "block";
-    document.querySelector(".dice-pic").src = `./images/dice-${dice}.png`;
-    scores[activePlayer] += dice;
-    document.querySelector(
-      `.player-${activePlayer}-current-score`
-    ).textContent = scores[activePlayer];
-  } else {
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    document.querySelector(".dice-pic").style.display = "none";
+  if (isPlaying) {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    if (dice !== 1) {
+      document.querySelector(".dice-pic").style.display = "block";
+      document.querySelector(".dice-pic").src = `./images/dice-${dice}.png`;
+      scores[activePlayer] += dice;
+      document.querySelector(
+        `.player-${activePlayer}-current-score`
+      ).textContent = scores[activePlayer];
+    } else {
+      nextPlayer();
+    }
   }
 });
+
+document.querySelector(".hold-dice").addEventListener("click", () => {
+  if (isPlaying) {
+    globalScores[activePlayer] += scores[activePlayer];
+    if (globalScores[activePlayer] >= winningScore) {
+      document.querySelector(`.player-${activePlayer}-name`).textContent =
+        "Winner";
+      isPlaying = false;
+    }
+    document.querySelector(`.player-${activePlayer}-global-score`).textContent =
+      globalScores[activePlayer];
+    nextPlayer();
+  }
+});
+
+document.querySelector(".new-game").addEventListener("click", () => {
+  globalScores[0] = 0;
+  globalScores[1] = 0;
+  scores[0] = 0;
+  scores[1] = 0;
+  activePlayer = 0;
+  document.querySelector(`.player-0-name`).classList.add("active");
+  document.querySelector(`.player-1-name`).classList.remove("active");
+  document.querySelector(`.player-0-current-score`).textContent = scores[0];
+  document.querySelector(`.player-1-current-score`).textContent = scores[1];
+  document.querySelector(`.player-0-global-score`).textContent =
+    globalScores[0];
+  document.querySelector(`.player-1-global-score`).textContent =
+    globalScores[1];
+
+  document.querySelector(`.player-0-name`).textContent = `Player 1`;
+  document.querySelector(`.player-1-name`).textContent = `Player 2`;
+});
+
+function nextPlayer() {
+  scores[activePlayer] = 0;
+  document.querySelector(`.player-${activePlayer}-current-score`).textContent =
+    scores[activePlayer];
+  document
+    .querySelector(`.player-${activePlayer}-name`)
+    .classList.remove("active");
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  if (isPlaying) {
+    document
+      .querySelector(`.player-${activePlayer}-name`)
+      .classList.add("active");
+  }
+  document.querySelector(".dice-pic").style.display = "none";
+}
